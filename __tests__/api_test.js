4b098fdb524b565e86e973e32c46f62abfd8d117
강동윤 <kdy1@outlook.kr>
2019-02-18 23:18:38 +0900

    Add test for swc-project/swc#226

--- __tests__/api_test.js
+++ __tests__/api_test.js
@@ -8,3 +8,18 @@ it('should handle minify', () => {
         minify: true,
     }).code.trim()).toBe("import foo,{bar}from'foo';");
 });
+
+it('should handle exportNamespaceFrom', () => {
+    const src = "export * as Foo from 'bar';";
+    const out = swc.transformSync(src, {
+        jsc: {
+            parser: {
+                syntax: "ecmascript",
+                exportNamespaceFrom: true,
+            }
+        }
+    });
+
+    expect(out.code).toContain("import * as _Foo from 'bar';");
+    expect(out.code).toContain("export { _Foo as Foo }");
+});
