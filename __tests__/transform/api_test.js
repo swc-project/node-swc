a52bbd82acee09de2b005155fe6a64f820c1cca6
강동윤 <kdy1@outlook.kr>
2019-06-03 19:18:35 +0900

    Add test for swc-project/swc#389

--- __tests__/transform/api_test.js
+++ __tests__/transform/api_test.js
@@ -1,66 +1,74 @@
+const swc = require("../../lib/index");
 
-const swc = require('../../lib/index');
+it("should handle minify", () => {
+  const src = '/* Comment */import foo, {bar} from "foo"';
 
-it('should handle minify', () => {
-    const src = '/* Comment */import foo, {bar} from "foo"';
-
-    expect(swc.transformSync(src, {
-        minify: true,
-    }).code.trim()).toBe("import foo,{bar}from'foo';");
+  expect(
+    swc
+      .transformSync(src, {
+        minify: true
+      })
+      .code.trim()
+  ).toBe("import foo,{bar}from'foo';");
 });
 
-it('should handle sourceMaps = false', () => {
-    const src = '/* Comment */import foo, {bar} from "foo"';
-    const out = swc.transformSync(src, {
-        sourceMaps: false,
-    });
+it("should handle sourceMaps = false", () => {
+  const src = '/* Comment */import foo, {bar} from "foo"';
+  const out = swc.transformSync(src, {
+    sourceMaps: false
+  });
 
-    expect(out.map).toBeFalsy();
+  expect(out.map).toBeFalsy();
 });
 
-it('should handle exportNamespaceFrom', () => {
-    const src = "export * as Foo from 'bar';";
-    const out = swc.transformSync(src, {
-        jsc: {
-            parser: {
-                syntax: "ecmascript",
-                exportNamespaceFrom: true,
-            }
-        }
-    });
+it("should handle exportNamespaceFrom", () => {
+  const src = "export * as Foo from 'bar';";
+  const out = swc.transformSync(src, {
+    jsc: {
+      parser: {
+        syntax: "ecmascript",
+        exportNamespaceFrom: true
+      }
+    }
+  });
 
-    expect(out.code).toContain("import * as _Foo from 'bar';");
-    expect(out.code).toContain("export { _Foo as Foo }");
+  expect(out.code).toContain("import * as _Foo from 'bar';");
+  expect(out.code).toContain("export { _Foo as Foo }");
 });
 
-it('should handle exportNamespaceFrom configured by .swcrc', () => {
-    const out = swc.transformFileSync(__dirname + '/../../issue-226/input.js');
+it("should handle exportNamespaceFrom configured by .swcrc", () => {
+  const out = swc.transformFileSync(__dirname + "/../../issue-226/input.js");
 
-    expect(out.code).toContain("import * as _Foo from 'bar';");
-    expect(out.code).toContain("export { _Foo as Foo }");
+  expect(out.code).toContain("import * as _Foo from 'bar';");
+  expect(out.code).toContain("export { _Foo as Foo }");
 });
 
-it('should handle jsc.target = es3', () => {
-    const out = swc.transformSync(`foo.default`, {
-        jsc: {
-            target: 'es3'
-        }
-    });
-    expect(out.code.trim()).toBe(`foo['default'];`)
+it("should handle jsc.target = es3", () => {
+  const out = swc.transformSync(`foo.default`, {
+    jsc: {
+      target: "es3"
+    }
+  });
+  expect(out.code.trim()).toBe(`foo['default'];`);
 });
 
-it('should handle jsc.target = es5', () => {
-    const out = swc.transformSync(`foo.default`, {
-        jsc: {
-            target: 'es5'
-        }
-    });
-    expect(out.code.trim()).toBe(`foo.default;`)
+it("should handle jsc.target = es5", () => {
+  const out = swc.transformSync(`foo.default`, {
+    jsc: {
+      target: "es5"
+    }
+  });
+  expect(out.code.trim()).toBe(`foo.default;`);
 });
 
+it("should handle react correctly", () => {
+  const out = swc.transformFileSync(__dirname + "/../../issue-351/input.js");
 
-it('should handle react correctly', () => {
-    const out = swc.transformFileSync(__dirname + '/../../issue-351/input.js');
+  expect(out.code).toContain(`.default.createElement('div', null);`);
+});
 
-    expect(out.code).toContain(`.default.createElement('div', null);`);
+it("should handle cjs imports", () => {
+  const out = swc.transformFileSync(__dirname + "/../../issue-389/input.js");
+
+  expect(out.code).toContain(`.default.bar = true`);
 });
