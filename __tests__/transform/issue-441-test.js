beace45cd98fb98ef13244a69ddfe6865b002d6f
강동윤 <kdy1997.dev@gmail.com>
2019-11-15 14:24:08 +0900

    Add a test for @swc-project/swc#441

---
+++ __tests__/transform/issue-441-test.js
@@ -0,0 +1,14 @@
+const swc = require("../../lib/index");
+
+it("should handle comments in return", () => {
+  const out = swc.transformSync(`
+  () => {
+    return (
+      // Interfering comment
+      foo.bar
+    );
+  };
+  `);
+
+  expect(out.code).toContain(`return(// Interfering`);
+});
