d21c3e6511b5a3cd2efa5b4999a665b8030d83de
강동윤 <kdy1997.dev@gmail.com>
2019-11-26 18:13:22 +0900

    Fix linux build

--- scripts/ci/linux/build.sh
+++ scripts/ci/linux/build.sh
@@ -9,46 +9,12 @@
 ####################
 set -e
 
-echo "Switching to node v$1 ($2)"
-echo "Intsalling node $1"
-
-# Bypasses https://github.com/neon-bindings/neon/issues/384
-echo 'Removing old files'
-
-if [[ -z "$APPVEYOR" ]] && [[ "$TRAVIS_OS_NAME" != "windows" ]] ; then
-    echo 'Mac os / linux works without hack'
-else
-    echo "Deleting artifacts.json"
-    rm -rf ./native/artifacts.json
-fi
-
-rm -rf ./native/index.node \
-    ./native/target/release/libffi.d* \
-    ./native/target/release/ffi.d* \
-    ./native/target/release/build/ffi* \
-    ./native/target/release/.fingerprint/neon* \
-    ./native/target/release/deps/ffi* \
-    ./native/target/release/deps/libffi* \
-    ./native/target/release/deps/neon* \
-    ./native/target/release/deps/libneon*
-echo 'Removed old files'
-
-export PATH="/c/nvm-root:/c/nodejs:$PATH"
-
-
-
-# if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then ls -al '/c/nodejs' ; fi
-# if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then ls -alL '/c/nodejs' ; fi
 which node && node --version
 which npm
 
 
 export RUSTFLAGS='--cfg procmacro2_semver_exempt --cfg parallel_queries' 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    rustup target add x86_64-unknown-linux-musl
-    export CARGO_BUILD_TARGET="x86_64-unknown-linux-musl"
-    export RUSTFLAGS="$RUSTFLAGS -C target-feature=-crt-static"
-fi
+export RUSTFLAGS="$RUSTFLAGS -C target-feature=-crt-static"
 
 
 # (cd native && cargo build --release --verbose)
@@ -58,12 +24,6 @@ npm install --ignore-scripts
 # Build it
 echo 'Building...'
 npx neon build --release 
-
-ls -al ./native/target/release
-
-ls -al ./native/target/release/deps
-
-ls -al ./native/target/release/build
 
 # Verify abi
 echo 'Verifying binding with jest...'
--- scripts/ci/linux/start-docker.sh
+++ scripts/ci/linux/start-docker.sh
@@ -4,7 +4,7 @@
 
 set -ex
 
-docker build -t node-rust -f "scripts/ci/linux/Dockerfile" .
+docker build -t node-rust-$2 -f "scripts/ci/linux/Dockerfile" .
 
 # sleep so our detached container with no long running process sits around to accept commands for a bit
-docker run --detach --name target -v "$(pwd)":/src -w /src node-rust sleep 999999999
+docker run --detach --name target-$2 -v "$(pwd)":/src -w /src node-rust-$2 sleep 999999999
