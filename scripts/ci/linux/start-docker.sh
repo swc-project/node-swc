45d598fc1d9168f93bc8d1210bc22e4492d97f05
강동윤 <kdy1997.dev@gmail.com>
2019-11-27 14:21:34 +0900

    rpm

--- scripts/ci/linux/start-docker.sh
+++ scripts/ci/linux/start-docker.sh
@@ -4,7 +4,7 @@
 
 set -eux
 
-docker build -t node-rust-$2 -f "scripts/ci/linux/Dockerfile" . --build-arg NODE_VERSION="$1"
+docker build -t node-rust-$2 -f "scripts/ci/linux/Dockerfile" . --build-arg NODE_VERSION="$3"
 
 # sleep so our detached container with no long running process sits around to accept commands for a bit
 docker run --detach --name target-$2 -v "$(pwd)":/src -w /src node-rust-$2 sleep 999999999
