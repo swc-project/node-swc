f167add38d5a1d95978b476de77e9d470945cd55
강동윤 <kdy1997.dev@gmail.com>
2019-12-28 22:20:59 +0900

    show banner

--- scripts/install.js
+++ scripts/install.js
@@ -3,13 +3,15 @@
  */
 
 var fs = require("fs"),
+  os = require("os"),
   eol = require("os").EOL,
   mkdir = require("mkdirp"),
   path = require("path"),
   swc = require("../src/extensions"),
   request = require("request"),
   log = require("npmlog"),
-  downloadOptions = require("./util/downloadoptions");
+  downloadOptions = require("./util/downloadoptions"),
+  env = process.env;
 
 /**
  * Download file, if succeeds save, if not delete
@@ -173,6 +175,59 @@ function checkAndDownloadBinary() {
     }
   });
 }
+
+var BANNER =
+  "\u001B[96mThank you for using swc (\u001B[94m https://github.com/swc-project/swc \u001B[96m): super-fast javascript and typescript files \u001B[0m\n\n" +
+  "\u001B[96mThe project needs your help! Please consider supporting swc on Open Collective: \u001B[0m\n" +
+  "\u001B[96m>\u001B[94m https://opencollective.com/swc \u001B[0m\n" +
+  "\u001B[96mAlso, the author of swc (\u001B[94m https://github.com/kdy1 \u001B[96m) is looking for a good job -)\u001B[0m\n";
+
+var ADBLOCK = is(env.ADBLOCK);
+var COLOR = is(env.npm_config_color);
+var DISABLE_OPENCOLLECTIVE = is(env.DISABLE_OPENCOLLECTIVE);
+var SILENT =
+  ["silent", "error", "warn"].indexOf(env.npm_config_loglevel) !== -1;
+var MINUTE = 60 * 1000;
+
+// you could add a PR with an env variable for your CI detection
+var CI = ["BUILD_NUMBER", "CI", "CONTINUOUS_INTEGRATION", "RUN_ID"].some(
+  function(it) {
+    return is(env[it]);
+  }
+);
+
+function is(it) {
+  return !!it && it !== "0" && it !== "false";
+}
+
+function isBannerRequired() {
+  if (ADBLOCK || CI || DISABLE_OPENCOLLECTIVE || SILENT) return false;
+  var file = path.join(os.tmpdir(), "core-js-banners");
+  var banners = [];
+  try {
+    var DELTA = Date.now() - fs.statSync(file).mtime;
+    if (DELTA >= 0 && DELTA < MINUTE * 3) {
+      banners = JSON.parse(fs.readFileSync(file, "utf8"));
+      if (banners.indexOf(BANNER) !== -1) return false;
+    }
+  } catch (error) {
+    banners = [];
+  }
+  try {
+    banners.push(BANNER);
+    fs.writeFileSync(file, JSON.stringify(banners), "utf8");
+  } catch (error) {
+    /* empty */
+  }
+  return true;
+}
+
+function showBanner() {
+  // eslint-disable-next-line no-console,no-control-regex
+  console.log(COLOR ? BANNER : BANNER.replace(/\u001B\[\d+m/g, ""));
+}
+
+if (isBannerRequired()) showBanner();
 
 /**
  * If binary does not exist, download it
