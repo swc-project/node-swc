4b4101652937780afd6abd28a37e439a3801525f
강동윤 <kdy1997.dev@gmail.com>
2019-12-28 23:15:12 +0900

    Add a test

--- __tests__/env/env_test.js
+++ __tests__/env/env_test.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 it("should handle targets in env", () => {
   const filename = path.resolve(
-    __dirname + "/../../tests/env-targets/input.js"
+    __dirname + "/../../tests/env/targets/input.js"
   );
   expect(swc.transformFileSync(filename).code.trim()).toContain(
     `const a of foo`
